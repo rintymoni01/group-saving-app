71be4ed157df70ff33bc76b45388fd4d66d09cca
rintymoni01 <rintymoni01@gmail.com>
2026-05-08 07:25:07 +0600

    Implement project architecture as requested

---
+++ lib/view/screens/splash/controller/splash_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class SplashController extends GetxController {
+  // Add controller logic for splash screen
+}
---
+++ lib/view/screens/splash/widgets/splash_specific_widgets.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashWidget extends StatelessWidget {
+  const SplashWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
