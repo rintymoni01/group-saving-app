9cb0bcb6ef07633ad327da7cae59695b21fa32ff
rintymoni01 <rintymoni01@gmail.com>
2026-05-08 07:34:26 +0600

    Update AppColors with design color palette

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Colors.blue;
-  // Add other colors here
+  static const Color primaryColor = Color(0xFF1A237E);
+  static const Color backgroundColor = Color(0xFF121212);
+  static const Color cardBackgroundColor = Colors.white;
+  static const Color fieldBorderColor = Color(0xFFE0E0E0);
+  static const Color greyColor = Color(0xFF757575);
+  static const Color lightGreyColor = Color(0xFFBDBDBD);
 }
