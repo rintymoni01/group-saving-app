39c41a42396f3821c9feedf1c72f33b0d4659c3b
rintymoni01 <rintymoni01@gmail.com>
2026-05-09 07:47:54 +0600

    MVC Refactor Step 3: Create SignUpController

---
+++ lib/view/screens/auth/sign_up/controller/sign_up_controller.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SignUpController extends GetxController {
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  
+  var agreeToTerms = false.obs;
+
+  void toggleTerms(bool? value) {
+    agreeToTerms.value = value ?? false;
+  }
+
+  @override
+  void onClose() {
+    nameController.dispose();
+    emailController.dispose();
+    phoneController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    super.onClose();
+  }
+}
