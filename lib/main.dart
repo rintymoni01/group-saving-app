916708f25d01434fe198de5ce7e5f1fc4cf477b4
rintymoni01 <rintymoni01@gmail.com>
2026-05-09 07:38:29 +0600

    Final: Link CompleteProfileScreen in main.dart for preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:group_saving_app/core/utils/app_colors.dart';
-import 'package:group_saving_app/view/screens/auth/screen/sign_up_screen.dart';
+import 'package:group_saving_app/view/screens/profile/screen/complete_profile_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
         useMaterial3: true,
       ),
-      home: const SignUpScreen(),
+      home: const CompleteProfileScreen(),
     );
   }
 }
