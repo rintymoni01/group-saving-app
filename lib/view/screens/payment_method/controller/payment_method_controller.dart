5b1bde5bfc7428be7ddf1d91aa1eb27809f087dd
rintymoni01 <rintymoni01@gmail.com>
2026-05-12 20:42:34 +0600

    P3: Add toggleBankForm method

--- lib/view/screens/payment_method/controller/payment_method_controller.dart
+++ lib/view/screens/payment_method/controller/payment_method_controller.dart
@@ -2,4 +2,8 @@ import 'package:get/get.dart';
 
 class PaymentMethodController extends GetxController {
   var isBankFormVisible = false.obs;
+
+  void toggleBankForm() {
+    isBankFormVisible.value = !isBankFormVisible.value;
+  }
 }
