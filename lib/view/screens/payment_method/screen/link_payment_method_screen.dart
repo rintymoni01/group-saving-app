816b2b7b2a5ceae55a2fc6884770c34b1debcc90
rintymoni01 <rintymoni01@gmail.com>
2026-05-12 20:42:46 +0600

    P4: Initialize LinkPaymentMethodScreen

---
+++ lib/view/screens/payment_method/screen/link_payment_method_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:group_saving_app/view/screens/payment_method/controller/payment_method_controller.dart';
+
+class LinkPaymentMethodScreen extends StatelessWidget {
+  const LinkPaymentMethodScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
