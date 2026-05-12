bc9b590f872a8376ce36057b648b7e695e2f5fdc
rintymoni01 <rintymoni01@gmail.com>
2026-05-12 20:51:45 +0600

    P24: Add text controllers to PaymentMethodController

--- lib/view/screens/payment_method/controller/payment_method_controller.dart
+++ lib/view/screens/payment_method/controller/payment_method_controller.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class PaymentMethodController extends GetxController {
   var isBankFormVisible = false.obs;
+
+  final routingController = TextEditingController();
+  final accountController = TextEditingController();
 
   void toggleBankForm() {
     isBankFormVisible.value = !isBankFormVisible.value;
